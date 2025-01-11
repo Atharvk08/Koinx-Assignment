e178c89fbd406e01b65907d5d6bbe3cc5d2467c6
Atharvk08 <atharva.kharapkar@gmail.com>
2025-01-11 13:28:22 +0530

    feat: Added the deviation api endpoint

--- server.js
+++ server.js
@@ -98,6 +98,42 @@ app.get("/stats", async (req, res) => {
   });
 });
 
+app.get("/deviation", async (req, res) => {
+  const coin = req.query.coin;
+  if (!coin) {
+    return res.status(400).send({ error: "Coin parameter is required" });
+  }
+  try {
+    const data = await Crypto.findOne({ coin });
+    if (!data) {
+      return res.status(404).send({ error: "Coin not found" });
+    }
+
+    const prices = data.timestamp.slice(-100).map((entry) => entry.price);
+
+    if (prices.length < 2) {
+      return res.status.send({
+        error: "not enough data to calculate deviation",
+      });
+    }
+
+    const deviation = calculateStandardDeviation(prices);
+    res.json({
+      deviation: parseFloat(deviation.toFixed(2)),
+    });
+  } catch (error) {
+    console.error("Error fetchin deviation: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const calculateStandardDeviation = (arr) => {
+  const mean = arr.reduce((a, b) => a + b, 0) / arr.length;
+  return Math.sqrt(
+    arr.reduce((a, b) => a + Math.pow(b - mean, 2), 0) / arr.length
+  );
+};
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
