bd0c4bb73b075eef7958448ee5df2145acf3b272
Atharvk08 <atharva.kharapkar@gmail.com>
2025-01-11 11:33:17 +0530

    feat: integrate Axios for fetching the cryptocurrencies data from CoinGecko API

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cron = require("node-cron");
-const axis = require("axios");
+const axios = require("axios");
+
+require("dotenv").config();
 
 const app = express();
 
+// Crypto currency Data Schema
+const cryptoSchema = new mongoose.Schema({
+  coin: String,
+  price: Number,
+  marketCap: Number,
+  change: Number,
+});
+
+// fetch data from the coin url
+const URL = process.env.CRYPTO_URL;
+
+async function fetchCryptoData() {
+  const coins = ["bitcoin", "matic-network", "ethereum"];
+
+  for (const coin of coins) {
+    queryURL =
+      URL +
+      `?ids=${coin}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=3`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": process.env.API_KEY,
+      },
+    };
+    const response = await axios.get(queryURL, options);
+    const data = response.data[coin];
+    console.log(data);
+  }
+}
+
+app.get("/coin", (req, res) => {
+  fetchCryptoData();
+});
+
 app.listen(3000, () => {
-  console.log("Server listening on port 5000");
+  console.log("Server listening on port 3000");
 });
